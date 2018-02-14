f27718b3c917e55c54515b241b3bfb6c9a80168e
m kishore daass <m kishore daass@LAPTOP-F9SA4CRT>
2018-02-14 18:06:47 +0530

    sample

---
+++ src/tests/demo.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class demo
+{
+	@Test
+	public void sample()
+	{
+		Reporter.log("HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII",true);
+	}
+	
+
+}
